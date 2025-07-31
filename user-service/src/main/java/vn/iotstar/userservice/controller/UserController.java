7a243411737ab600d1839ae00627cdd5af777ad8
81quanghuy <20110650@student.hcmute.edu.vn>
2025-07-31 14:37:38 +0700

    #1 add retry and rate limiter in user-service

--- user-service/src/main/java/vn/iotstar/userservice/controller/UserController.java
+++ user-service/src/main/java/vn/iotstar/userservice/controller/UserController.java
@@ -1,8 +1,12 @@
 package vn.iotstar.userservice.controller;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.iotstar.utils.constants.GenericResponse;
@@ -19,6 +23,7 @@ import static vn.iotstar.userservice.util.Constants.PARAM_USER_ID;
 public class UserController {
 
     private final UserService userService;
+    private final Environment environment;
 
     /**
      * Register a new user
@@ -66,5 +71,42 @@ public class UserController {
     public ResponseEntity<GenericResponse> deleteUserById(@PathVariable(PARAM_USER_ID) String userId) {
         log.info("Deleting user by ID: {}", userId);
         return userService.deleteUserById(userId);
+    }
+
+    @Retry(name = "getContactInfo",fallbackMethod = "getContactInfoFallback")
+    @GetMapping("/contact-info")
+    public ResponseEntity<GenericResponse> getContactInfo() {
+
+        log.info("Fetching contact information");
+        GenericResponse response = new GenericResponse();
+        response.setSuccess(true);
+        response.setMessage("Contact information retrieved successfully.");
+        response.setResult("For support, please contact us at ");
+        response.setStatusCode(HttpStatus.OK.value());
+        return ResponseEntity.ok(response);
+    }
+
+    public ResponseEntity<GenericResponse> getContactInfoFallback(Exception e) {
+        log.error("Error fetching contact information: {}", e.getMessage());
+        GenericResponse response = new GenericResponse();
+        response.setSuccess(false);
+        response.setMessage("Failed to fetch contact information. Please try again later.");
+        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+
+    @RateLimiter(name= "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Exception e) {
+        log.error("Error fetching Java version: {}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Failed to fetch Java version. Please try again later.");
     }
 }
